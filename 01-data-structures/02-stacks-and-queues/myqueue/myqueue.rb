708607934b4767db0c0dc25abe6e26c698c2bd14
Drew-Mace <dremac1@live.com>
2017-08-05 11:30:30 -0400

    stack/queue

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,39 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
+    @tailNum = -1
   end
 
   def enqueue(element)
+    @tailNum += 1
+    @queue[@tailNum] = element
+    @head = @queue[0]
+    @tail = @queue[@tailNum]
   end
 
   def dequeue
+    temp = @head
+    (0...@queue.length-1).each do |i|
+      @queue[i] = @queue[i+1]
+    end
+    @queue[@tailNum] = nil
+    @head = @queue[0]
+    @tailNum -= 1
+    if @tailNum < 0
+      @head = nil
+      @tail = nil
+    else
+      @tail = @queue[@tailNum]
+    end
+    return temp
   end
 
   def empty?
+    if @head === nil
+    return true
+    else 
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,24 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @pointer = -1
   end
 
   def push(item)
+    @pointer += 1
+    @top = item
+    @stack[@pointer] = @top
   end
 
   def pop
+    temp = @top
+    @stack[@pointer] = nil
+    @pointer -= 1
+    @top = @stack[@pointer]
+    return temp
   end
 
   def empty?
+    @top.nil?
   end
 end

e11fa159ba64a2648b89b64f6085b40c4f428a88
Drew-Mace <dremac1@live.com>
2017-09-03 13:57:46 -0400

    binary_searches

--- 02-algorithms/binary_search_recursive.rb
+++ 02-algorithms/binary_search_recursive.rb
@@ -9,6 +9,7 @@ def binary_search_recursive(collection, value)
     mid
   elsif collection[mid] < value
     binary_search_recursive(collection[(mid+1)..high], value)
+    return collection[value]
   else
     binary_search_recursive(collection[low..mid], value)
     return collection[value]
@@ -18,5 +19,5 @@ end
 
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-puts binary_search_recursive(array, 7)
+puts binary_search_recursive(array, 14)
 puts binary_search_recursive(array, 43)
